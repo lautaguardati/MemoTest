d806bc703eaf47df80717fc2624bd709ea804cac
lautaguardati <lautaguardati@gmail.com>
2024-06-10 14:46:06 -0300

    Método de barajar y colocar banderas
    Se usa el método de Fisher-Yates para mezclar aleatoriamente las banderas y que aparezcan en distintos sitios. Se las coloca en el "name" de cada elemento de imagen del html

---
+++ main.js
@@ -0,0 +1,30 @@
+
+let banderas = ["bandera-argentina", "bandera-bolivia", "bandera-brasil", "bandera-chile",
+    "bandera-paraguay", "bandera-peru", "bandera-uruguay", "bandera-venezuela"]
+
+banderas.forEach((bandera) => {
+    banderas.push(bandera)
+})
+
+
+//Aplicamos el barajado de Fisher-Yates
+function barajar(banderas) {
+    let m = banderas.length, t, i;
+    while (m) {
+        i = Math.floor(Math.random() * m--)
+
+        t = banderas[m];
+        banderas[m] = banderas[i];
+        banderas[i] = t
+    }
+    colocarBanderas()
+}
+
+function colocarBanderas() {
+    i = 1;
+    banderas.forEach((bandera) => {
+        document.querySelector("#cuadro-" + i).setAttribute("name", "img/" + bandera + ".jpg")
+        i++;
+    })
+}
+
